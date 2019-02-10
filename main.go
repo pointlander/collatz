d0922ae8df46a40302626caf399b45fb29a70073
Andrew Snodgrass <git@pointlander.info>
2019-02-10 12:49:32 -0700

    Refactor

--- main.go
+++ main.go
@@ -16,28 +16,31 @@ var (
 )
 
 var (
-	number = flag.Int64("number", 13, "starting number")
+	number = flag.String("number", "13", "starting number")
 	brute = flag.Bool("brute", false, "try a bunch of numbers")
 )
 
-func collatz(number int64) {
-	i, series, z := big.NewInt(number), make([]big.Int, 0, 256), big.Int{}
-	z.Set(i)
-	series = append(series, z)
-	fmt.Println(i)
+func collatz(i *big.Int) []big.Int {
+	series := make([]big.Int, 0, 256)
+	cp := func() (z big.Int) {
+		z.Set(i)
+		return z
+	}
+	series = append(series, cp())
 	for one.Cmp(i) != 0 {
 		if i.Bit(0) == 0 {
 			i.Rsh(i, 1)
 		} else {
-			i.Mul(three, i).Add(one, i)
+			z := cp()
+			i.Lsh(i, 1).SetBit(i, 0, 1).Add(i, &z)
 		}
-		z = big.Int{}
-		z.Set(i)
-		series = append(series, z)
-		if !*brute {
-			fmt.Println(i)
-		}
+		series = append(series, cp())
 	}
+
+	return series
+}
+
+func sumProductTest(series []big.Int) {
 	length := len(series)
 	sums, products := make(map[string]int, length*length), make(map[string]int, length*length)
 	for _, x := range series {
@@ -50,7 +53,7 @@ func collatz(number int64) {
 		}
 	}
 	max := (length * (length + 1))/2
-	fmt.Println(max, float64(len(sums)) / float64(max), float64(len(products)) / float64(max))
+	fmt.Println(max, float64(len(sums)) / float64(max), float64(len(products)) / float64(max))	
 }
 
 func main() {
@@ -58,10 +61,20 @@ func main() {
 
 	if *brute {
 		for i := 1; i < 256; i++ {
-			collatz(int64(i))
+			series := collatz(big.NewInt(int64(i)))
+			sumProductTest(series)
 		}
 		return
 	}
 
-	collatz(*number)
+	i := big.Int{}
+	_, ok := i.SetString(*number, 10)
+	if !ok {
+		panic("invalid number")
+	}
+	series := collatz(&i)
+	for _, item := range series {
+		fmt.Println(&item)
+	}
+	sumProductTest(series)
 }
