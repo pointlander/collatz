61a40fa3ac22582efec149ba9c91af80267deb9c
Andrew Snodgrass <git@pointlander.info>
2019-02-14 23:10:38 -0700

    Added new series

--- main.go
+++ main.go
@@ -11,13 +11,20 @@ import (
 )
 
 var (
-	one = big.NewInt(1)
+	one   = big.NewInt(1)
+	two   = big.NewInt(2)
 	three = big.NewInt(3)
+	a     = &big.Int{}
+	b     = &big.Int{}
 )
 
 var (
-	number = flag.String("number", "13", "starting number")
-	brute = flag.Bool("brute", false, "try a bunch of numbers")
+	number     = flag.String("number", "13", "starting number")
+	brute      = flag.Bool("brute", false, "try a bunch of numbers")
+	aa         = flag.String("a", "2", "number series parameter")
+	bb         = flag.String("b", "3", "number series parameter")
+	arithmetic = flag.Bool("arithmetic", false, "use arithmetic integers for series")
+	geometric  = flag.Bool("geometric", false, "use geometric integers for series")
 )
 
 func collatz(i *big.Int) []big.Int {
@@ -40,6 +47,24 @@ func collatz(i *big.Int) []big.Int {
 	return series
 }
 
+func arithmeticSeries() []big.Int {
+	series := make([]big.Int, 256)
+	for i := range series {
+		x := &series[i]
+		x.SetInt64(int64(i)).Mul(b, x).Add(a, x)
+	}
+	return series
+}
+
+func geometricSeries() []big.Int {
+	series := make([]big.Int, 256)
+	for i := range series {
+		x := &series[i]
+		x.SetInt64(int64(i)).Exp(b, x, nil).Mul(a, x)
+	}
+	return series
+}
+
 func sumProductTest(series []big.Int) {
 	length := len(series)
 	sums, products := make(map[string]int, length*length), make(map[string]int, length*length)
@@ -52,23 +77,49 @@ func sumProductTest(series []big.Int) {
 			products[product.Text(2)]++
 		}
 	}
-	max := (length * (length + 1))/2
-	fmt.Println(max, float64(len(sums)) / float64(max), float64(len(products)) / float64(max))	
+	max := (length * (length + 1)) / 2
+	fmt.Println(max, float64(len(sums))/float64(max), float64(len(products))/float64(max))
 }
 
 func main() {
 	flag.Parse()
 
+	_, ok := a.SetString(*aa, 10)
+	if !ok {
+		panic("invalid string for parameter a")
+	}
+	_, ok = b.SetString(*bb, 10)
+	if !ok {
+		panic("invalid string for parameter b")
+	}
+
 	if *brute {
-		for i := 1; i < 256; i++ {
+		for i := 1; i < 1024; i++ {
 			series := collatz(big.NewInt(int64(i)))
 			sumProductTest(series)
 		}
 		return
 	}
 
+	if *arithmetic {
+		series := arithmeticSeries()
+		for _, item := range series {
+			fmt.Println(&item)
+		}
+		sumProductTest(series)
+		return
+	}
+	if *geometric {
+		series := geometricSeries()
+		for _, item := range series {
+			fmt.Println(&item)
+		}
+		sumProductTest(series)
+		return
+	}
+
 	i := big.Int{}
-	_, ok := i.SetString(*number, 10)
+	_, ok = i.SetString(*number, 10)
 	if !ok {
 		panic("invalid number")
 	}
